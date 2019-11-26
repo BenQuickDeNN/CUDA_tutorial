52d452c6efa07186b076ac4b65d0981d1c2e2be3
BenQuickDeNN <2413269761@qq.com>
2019-11-26 22:41:01 +0800

    添加check_gpuinfo

---
+++ check_gpuinfo/build.sh
@@ -0,0 +1,1 @@
+nvcc -x cu check_gpuinfo.cu -o check_gpuinfo
---
+++ check_gpuinfo/check_gpuinfo.cu
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+/**
+ * @brief display gpu information
+ * @param devProp device
+*/
+void dispGPUInfo(const cudaDeviceProp& devProp);
+
+/**
+ * @brief display gpu information
+ * @param dev_id gpu id
+ * @return GPU information
+*/
+cudaDeviceProp getGPUInfo(const unsigned int& dev_id);
+
+/**
+ * @brief main entry
+ * @return exit status
+*/
+int main(int argc, char** argv)
+{
+	// display the infomation of gpu0
+	dispGPUInfo(getGPUInfo(0));
+
+	return 0;
+}
+
+cudaDeviceProp getGPUInfo(const unsigned int& dev_id)
+{
+	std::printf("----------------GPU----------------\r\n");
+	cudaDeviceProp devProp;
+	cudaGetDeviceProperties(&devProp, dev_id);
+	return devProp;
+}
+void dispGPUInfo(const cudaDeviceProp& devProp)
+{
+	std::printf("ʹGPU name: %s\r\n", devProp.name);
+	std::printf("number of SMs: %d\r\n", devProp.multiProcessorCount);
+	std::printf("warp size: %d\r\n", devProp.warpSize);
+	std::printf("max number of thread per SM: %d\r\n", devProp.maxThreadsPerMultiProcessor);
+	std::printf("number of warp per SM: %d\r\n", devProp.maxThreadsPerMultiProcessor / devProp.warpSize);
+}
